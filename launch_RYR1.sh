69d58d2f66a7822f21194a4a7d9310c87a150d5b
Gavin Monahan <gavin.monahan@gmail.com>
2023-01-12 06:32:40 +0000

    add launch sctript

---
+++ launch_RYR1.sh
@@ -0,0 +1,6 @@
+bash run_RYR1.sh MGXZ22473_pass.fastq.gz 2>&1 | tee MGXZ22473_2.log
+#bash run_RYR1.sh MGXZ22474_pass.fastq.gz 2>&1 | tee MGXZ22474_2.log
+#bash run_RYR1.sh MGXZ22475_pass.fastq.gz 2>&1 | tee MGXZ22475_2.log
+#bash run_RYR1.sh MGXZ22476_pass.fastq.gz 2>&1 | tee MGXZ22476_2.log
+#bash run_RYR1.sh MGXZ22477_pass.fastq.gz 2>&1 | tee MGXZ22477_2.log
+#bash run_RYR1.sh MGXZ22478_pass.fastq.gz 2>&1 | tee MGXZ22478_2.log
